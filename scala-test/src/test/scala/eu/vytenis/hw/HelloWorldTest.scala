0ee2971527e409a906e86513f41d39382e213446
Vytenis Bivainis <monrealis@gmail.com>
2015-02-11 00:24:15 +0200

    Add HelloWorldTest.

---
+++ scala-test/src/test/scala/eu/vytenis/hw/HelloWorldTest.scala
@@ -0,0 +1,22 @@
+package eu.vytenis.hw
+
+import org.scalatest._
+
+import scala.collection.mutable
+
+class HelloWorldTest extends FlatSpec with Matchers {
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new mutable.Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be(2)
+    stack.pop() should be(1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new mutable.Stack[Int]
+    a[NoSuchElementException] should be thrownBy {
+      emptyStack.pop()
+    }
+  }
+}
