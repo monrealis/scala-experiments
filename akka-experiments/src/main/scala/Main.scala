979cf601ae64ab467e393fb95470b2dc931e3c48
Vytenis Bivainis <monrealis@gmail.com>
2021-03-05 00:07:04 +0200

    Add some lines

--- akka-experiments/src/main/scala/Main.scala
+++ akka-experiments/src/main/scala/Main.scala
@@ -1,8 +1,21 @@
 package eu.vytenis.akka
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 
 object Main extends App {
-  var as = ActorSystem("MyTest")
+  val as = ActorSystem("MyTest")
   println(as.name)
+  val a1 = as.actorOf(Props[MyActor])
+  val a2 = as.actorOf(Props[MyActor], "aaa")
+  a1 ! "Hi"
+  a2 ! "Hi hi"
+  println(a1)
+
+  class MyActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(s"Got $message");
+    }
+  }
+
+  def z(a: Int)(implicit b: Int, c: Int) = a + b
 }
